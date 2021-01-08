40abe7f804cfa8f1eb640e25c27739b7d6f06ad0
mwilhelmsson <micke@nevyn.se>
2021-01-08 15:57:30 +0100

    some basic configuration done

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
 
 (package-initialize)
 (unless package-archive-contents
-  (packages-refresh-contents))
+  (package-refresh-contents))
 
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
@@ -30,5 +30,38 @@
 (set-face-attribute 'default nil :font "DejaVu Sans Mono" :height 100)
 
 ;; Theming
-(load-theme 'tango-dark t)
+(load-theme 'wombat t)
 
+;; Diminish
+(use-package diminish) ; Tell packages I don't care about their minor mode
+
+;; Ivy and Counsel
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+	 :map ivy-minibuffer-map
+	 ("TAB" . ivy-alt-done)
+	 ("C-l" . ivy-alt-done)
+	 ("C-j" . ivy-next-line)
+	 ("C-k" . ivy-previous-line)
+	 :map ivy-switch-buffer-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-l" . ivy-done)
+	 ("C-d" . ivy-switch-buffer-kill)
+	 :map ivy-reverse-i-search-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+
+;; all-the-icons
+(use-package all-the-icons)
+
+;; Doom modeline
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 15)))
+
+;; Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
