95b873e2809d082d78d2ead13f6bb694f422c03b
mwilhelmsson <micke@nevyn.se>
2021-01-09 07:27:29 +0100

    Beginning of EXWM-config

--- init.el
+++ init.el
@@ -18,16 +18,3 @@
   :ensure org-plus-contrib)
 
 (org-babel-load-file "~/.emacs.d/Emacs.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(org-plus-contrib which-key use-package rainbow-delimiters org-bullets ivy-rich hydra helpful general forge evil-magit evil-collection doom-themes doom-modeline diminish counsel-projectile command-log-mode all-the-icons-dired)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
